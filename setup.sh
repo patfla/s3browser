82c763f871e425d83a3461f3656894419af8989d
Patrick Flaherty <patfla@DESKTOP-930EG0A>
2025-05-29 10:52:27 -0700

    move pip install cython setuptools inside venv

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 # Update and install system dependencies
 sudo apt update
 sudo apt install -y python3-pip python3-setuptools git zip unzip openjdk-21-jdk
-pip install cython setuptools
 
 
 # Optional but recommended: create and activate a virtual environment
 python3 -m venv buildozer-env
 source buildozer-env/bin/activate
+pip install cython setuptoolss
 
 # Install Buildozer using pip
 pip install --upgrade pip
