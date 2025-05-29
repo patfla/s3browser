69269c0517aa03c5bf8eddcc3b0705ae27a9c2c5
Patrick Flaherty <patfla@DESKTOP-930EG0A>
2025-05-29 11:47:38 -0700

    add dependencies from buildozer.spec

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt install -y python3-pip python3-setuptools git zip unzip openjdk-21-jdk
 # Optional but recommended: create and activate a virtual environment
 python3 -m venv buildozer-env
 source buildozer-env/bin/activate
-pip install cython setuptools
+pip install cython setuptools python-for-android kivy boto3 botocore jmespath idna charset_normalizer certifi,openssl python-dateutil s3transfer plyer
 
 # Install Buildozer using pip
 pip install --upgrade pip
