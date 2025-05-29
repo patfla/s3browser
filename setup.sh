455efc20d6109feadd5918e8c2519f67d37b7b81
patfla <patf@well.com>
2025-05-28 17:27:30 -0700

    Reorder steps in setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-# Update and install system dependencies
-sudo apt update
-sudo apt install -y python3-pip python3-setuptools git zip unzip openjdk-11-jdk
-
 # Optional but recommended: create and activate a virtual environment
 python3 -m venv buildozer-env
 source buildozer-env/bin/activate
+
+# Update and install system dependencies
+sudo apt update
+sudo apt install -y python3-pip python3-setuptools git zip unzip openjdk-11-jdk
 
 # Install Buildozer using pip
 pip install --upgrade pip
