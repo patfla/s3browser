632a74df8cfde15bbeb02f8fa168d03119e107a8
patfla <patf@well.com>
2025-05-28 17:45:00 -0700

    Install dependencies before venv

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Update and install system dependencies
 sudo apt update
-sudo apt install -y python3-pip python3-setuptools git zip unzip openjdk-11-jdk
+sudo apt install -y python3-pip python3-setuptools python3-venv git zip unzip openjdk-11-jdk
 
 # Optional but recommended: create and activate a virtual environment
 python3 -m venv buildozer-env
