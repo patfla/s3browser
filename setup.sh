42898b1c47964dd926c117a4d227dc7b33617387
Patrick Flaherty <patfla@DESKTOP-930EG0A.localdomain>
2025-05-29 14:48:25 -0700

    add autoconf automake libtool

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 
 # Update and install system dependencies
 sudo apt update
-sudo apt install -y python3-pip python3-setuptools git zip unzip openjdk-21-jdk
+sudo apt install -y python3-pip python3-setuptools git zip unzip openjdk-21-jdk autoconf automake libtool
 
 
 # Optional but recommended: create and activate a virtual environment
@@ -11,6 +11,8 @@ python3 -m venv buildozer-env
 source buildozer-env/bin/activate
 pip install --upgrade pip
 pip install cython setuptools python-for-android kivy boto3 botocore jmespath idna charset_normalizer certifi pyopenssl python-dateutil s3transfer plyer
+
+
 
 # Install Buildozer using pip
 pip install buildozer
