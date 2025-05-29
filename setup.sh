d245314329e47cf0268ed0b67b86b13b7d3a3aff
Patrick Flaherty <patfla@DESKTOP-930EG0A>
2025-05-28 17:17:47 -0700

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# Update and install system dependencies
+sudo apt update
+sudo apt install -y python3-pip python3-setuptools git zip unzip openjdk-11-jdk
+
+# Optional but recommended: create and activate a virtual environment
+python3 -m venv buildozer-env
+source buildozer-env/bin/activate
+
+# Install Buildozer using pip
+pip install --upgrade pip
+pip install buildozer
+
